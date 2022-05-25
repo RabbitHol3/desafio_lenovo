66938169bc65ed20a9b542e976526c6f5418fbd9
GGMaster <riilckgg@gmail.com>
2022-05-25 18:51:54 -0300

    .

--- run_robot.py
+++ run_robot.py
@@ -4,32 +4,6 @@ from django.core.management import call_command
 from multiprocessing import Process
 from robo.main import Robot
 
-def init_django():
-    import os
-    import django
-    from django.conf import settings
-
-    if settings.configured:
-        return
-
-    settings.configure(
-        INSTALLED_APPS=[
-            'api',
-        ],
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': 'db.sqlite3',
-            }
-        },
-        DEFAULT_AUTO_FIELD = 'django.db.models.AutoField',
-        TIME_ZONE = 'America/Sao_Paulo',
-        USE_TZ = True
-    )
-    django.setup()
-
-
-
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desafio_lenovo.settings')
     
